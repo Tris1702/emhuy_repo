a9df1d5ed948e64eb204cdbf7594c712e7a15358
Quach Le Hai Anh <haianhlequach@gmail.com>
2023-10-28 14:56:51 +0700

    push search.dart first time

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -8,11 +8,56 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  List<String> allItems = [
+    'Di tích 1',
+    'Di tích 2',
+    'Di tích 3',
+    'Di tích 4',
+    'Di tích 5',
+  ];
+
+  List<String> searchResults = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text(SearchPage.title),
-    ));
+      appBar: AppBar(
+        title: Text(SearchPage.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              onChanged: (query) {
+                onSearchTextChanged(query);
+              },
+              decoration: InputDecoration(
+                labelText: 'Tìm kiếm',
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: searchResults.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(searchResults[index],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onSearchTextChanged(String query) {
+    setState(() {
+      searchResults = allItems
+          .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
   }
 }
