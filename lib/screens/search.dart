583c2f818f9d5a104c9bba9a261da144f823401d
Quach Le Hai Anh <haianhlequach@gmail.com>
2023-10-28 16:01:50 +0700

    push 2

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+class CulturalHeritage {
+  final String name;
+  final String description;
+  final String imageUrl;
+  final String address;
+
+  CulturalHeritage({
+    required this.name,
+    required this.description,
+    required this.imageUrl,
+    required this.address,
+  });
+}
+
 class SearchPage extends StatefulWidget {
   static const title = 'Search';
 
@@ -8,27 +22,58 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-  List<String> allItems = [
-    'Di tích 1',
-    'Di tích 2',
-    'Di tích 3',
-    'Di tích 4',
-    'Di tích 5',
+  List<CulturalHeritage> allItems = [
+    CulturalHeritage(
+      name: 'Chùa Tam Chúc',
+      description: 'Ngôi chùa lớn nhất Đông Nam Á',
+      imageUrl: 'https://ik.imagekit.io/tvlk/blog/2023/05/chua-tam-chuc-12.jpg?tr=dpr-2,w-675', // URL hình ảnh thực tế
+      address: 'Hà Nam',
+    ),
+    CulturalHeritage(
+      name: 'Quốc Tử Giám',
+      description: 'Trường Đại học đầu tiên của Việt Nam',
+      imageUrl: 'https://ik.imagekit.io/tvlk/blog/2022/08/van-mieu-quoc-tu-giam-1-1024x683.jpg?tr=dpr-2,w-675', // URL hình ảnh thực tế
+      address: 'Hà Nội',
+    ),
+    // Thêm các di tích khác vào danh sách với URL hình ảnh thực tế.
   ];
 
-  List<String> searchResults = [];
+  List<CulturalHeritage> searchResults = [];
+  final TextEditingController _searchController = TextEditingController();
+
+  void onSearchTextChanged(String query) {
+    setState(() {
+      searchResults = allItems
+          .where((heritage) =>
+      heritage.name.toLowerCase().contains(query.toLowerCase()) ||
+          heritage.description.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
+  void clearSearch() {
+    _searchController.clear();
+    onSearchTextChanged('');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(SearchPage.title),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.clear), // Biểu tượng "X" để xóa nội dung tìm kiếm
+            onPressed: clearSearch,
+          ),
+        ],
       ),
       body: Column(
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
+              controller: _searchController,
               onChanged: (query) {
                 onSearchTextChanged(query);
               },
@@ -41,9 +86,11 @@ class _SearchPageState extends State<SearchPage> {
             child: ListView.builder(
               itemCount: searchResults.length,
               itemBuilder: (context, index) {
+                final culturalHeritage = searchResults[index];
                 return ListTile(
-                  title: Text(searchResults[index],
-                  ),
+                  leading: Image.network(culturalHeritage.imageUrl),
+                  title: Text(culturalHeritage.name),
+                  subtitle: Text(culturalHeritage.address),
                 );
               },
             ),
@@ -51,13 +98,5 @@ class _SearchPageState extends State<SearchPage> {
         ],
       ),
     );
-  }
-
-  void onSearchTextChanged(String query) {
-    setState(() {
-      searchResults = allItems
-          .where((item) => item.toLowerCase().contains(query.toLowerCase()))
-          .toList();
-    });
   }
 }
