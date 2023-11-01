4244fd6af0881402c63c5fe423909a22efc74af7
nongnong10 <jirachi@gmail.com>
2023-11-01 10:26:30 +0700

    [HOME] Add structure

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,9 +10,48 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-      title: Text(HomePage.title),
-    ));
+    return Container(
+      //White
+      color: Color(0xFFFFFFFF),
+      child: Scaffold(
+        //Transparent
+        backgroundColor: Color(0x00000000),
+        body: Column(
+          children: <Widget> [
+            SizedBox(
+              height: MediaQuery.of(context).padding.top,
+            ),
+            getAppBarUI(),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  child: Column(
+                    children: <Widget>[
+                      getStudyingFlashcardUI(),
+                      getNewFlashcardUI(),
+                      Flexible(child: getAllFlashcardUI()),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getAppBarUI() {
+    return Text("App Bar");
+  }
+  Widget getStudyingFlashcardUI() {
+    return Text("Studying");
+  }
+  Widget getNewFlashcardUI() {
+    return Text("New");
+  }
+  Widget getAllFlashcardUI() {
+    return Text("All");
   }
 }
