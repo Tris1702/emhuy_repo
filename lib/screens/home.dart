0ab50773db5168d2d3f9c68ee09e17687e064644
nongnong10 <jirachi@gmail.com>
2023-11-01 13:49:28 +0700

    [HOME] Add AppBar UI

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -43,7 +43,50 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget getAppBarUI() {
-    return Text("App Bar");
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0, left: 18, right: 18),
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              width: 60,
+              height: 60,
+              child: Image.asset('assets/home/userImage.png'),
+            ),
+          ),
+          const Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Welcome",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      height: 1.5,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 15,
+                      letterSpacing: 0.3,
+                      color: Color(0xFF3A5160), //grey
+                    ),
+                  ),
+                  Text(
+                    "User123",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      letterSpacing: 0.3,
+                      color: Color(0xFF17262A), //darker
+                    ),
+                  ),
+                ],
+              ),
+          )
+        ],
+      ),
+    );
   }
   Widget getStudyingFlashcardUI() {
     return Text("Studying");
